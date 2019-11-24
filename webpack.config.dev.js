b2cadcbd85196df4455e349ed8ceb102c1189eb6
one-more <dmni3865@gmail.com>
2019-11-24 14:29:36 -0800

    serve content from public

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,7 @@ const config = require('./webpack.config.common');
 module.exports = merge(config, {
     mode: 'development',
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: path.join(__dirname, 'public'),
         compress: true,
         port: process.env.PORT || 3000,
         open: true,
